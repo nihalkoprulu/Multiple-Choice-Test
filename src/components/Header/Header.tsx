b5842e4994a695dd17af5a181938fd16ac837b3c
NihalKoprulu <nihalkoprulu@gmail.com>
2024-10-18 17:53:37 +0100

    updated logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,11 @@
 import { FC } from "react";
 import { HeaderContainer, LogoContainer } from "./styled";
 import Logo from "assets/icons/Logo";
-import { useNavigate } from "react-router";
 
 const Header: FC = () => {
-  const navigate = useNavigate();
-  const clickHandler: () => void = () => navigate("/");
-
   return (
     <HeaderContainer>
-      <LogoContainer onClick={() => clickHandler()}>
+      <LogoContainer>
         <Logo />
       </LogoContainer>
     </HeaderContainer>
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,12 +1,11 @@
-
-import { themeColors } from 'assets/theme/style';
 import styled from 'styled-components';
+import { themeColors } from 'assets/theme/style';
 
 export const HeaderContainer = styled.div`
     display: flex;
     width: 100%;
     align-items: center;
-    padding: 20px 0;
+    padding: 20px;
     background-color: ${themeColors.white};
     box-shadow: rgba(0, 0, 0, 0.05) 0px 4px 6px;  
 `;
